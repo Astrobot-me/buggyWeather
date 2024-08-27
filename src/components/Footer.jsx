5b3204bde225ffb048c3e42ec44a31376a87be08
astrobot-me <autobot268@gmail.com>
2024-08-28 00:54:55 +0530

    Link add on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,15 @@ const Footer = () => {
     <footer className="footer p-10 bg-base-300 text-base-content">
       <div>
         <span className="footer-title">Services</span> 
-        <a className="link link-hover">Branding</a> 
-        <a className="link link-hover">Design</a> 
-        <a className="link link-hover">Marketing</a> 
-        <a className="link link-hover">Advertisement</a>
+        <a className="link link-hover" href='/getweather'>Weather</a> 
+        <a className="link link-hover" href='/'>Home</a> 
       </div> 
       <div>
-        <span className="footer-title">Company</span> 
-        <a className="link link-hover">About us</a> 
-        <a className="link link-hover">Contact</a> 
-        <a className="link link-hover">Jobs</a> 
-        <a className="link link-hover">Press kit</a>
+        <span className="footer-title">Aditya ( Astrobot )</span> 
+        <a className="link link-hover" href='https://x.com/astrobot_me' target='_blank'> Twitter</a> 
+        <a className="link link-hover" href='https://github.com/Astrobot-me/buggyWeather' target='_blank'> Github</a> 
+        <a className="link link-hover" href='https://x.com/astrobot_me' target='_blank'> Project</a> 
+        <a className="link link-hover" href='https://linkedin.com/in/astro-adityaraj/' target='_blank'> Linked In</a> 
       </div> 
       <div>
         <span className="footer-title">Legal</span> 
@@ -24,14 +22,14 @@ const Footer = () => {
         <a className="link link-hover">Cookie policy</a>
       </div>
       <div>
-        <span className="footer-title">Newsletter</span> 
+        <span className="footer-title">Weather Updates</span> 
         <div className="form-control w-80">
           <label className="label">
             <span className="label-text">Enter your email address</span>
           </label> 
           <div className="relative">
-            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
-            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" disabled/> 
+            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none" disabled>Subscribe</button>
           </div>
         </div>
       </div>
