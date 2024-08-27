75f94765308b77b006452b0cf37556ef8733fe8e
astrobot-me <autobot268@gmail.com>
2024-08-27 22:05:31 +0530

    Homepage changes to add icon + typing animation not login state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,19 +6,19 @@ import PreviousWeather from "../components/PreviousWeather";
 import CardFrame from "../components/CardFrame";
 import WeatherStats from "../components/WeatherStats";
 import useGetWeather from "../services/useGetWeather";
-
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
     const userData = useSelector((state) => state.auth.userData)
     const authStatus = useSelector((state) => state.auth.status)
-    const [place, setPlace] = useState(useSelector((state)=>state.auth.userConfig?.defaultPlace))
+    const [place, setPlace] = useState(useSelector((state) => state.auth.userConfig?.defaultPlace))
     // const [latitude,setLatitude] = useState(null)
     // const [longitude,setLongitude] = useState(null)
 
-    const lat = useSelector((state)=>state.auth?.userConfig?.defaultLat);
-    const long = useSelector((state)=>state.auth?.userConfig?.defaultLong);
+    const lat = useSelector((state) => state.auth?.userConfig?.defaultLat);
+    const long = useSelector((state) => state.auth?.userConfig?.defaultLong);
 
-    const weather = useGetWeather(lat, long,authStatus) || null;
+    const weather = useGetWeather(lat, long, authStatus) || null;
     const options = {
         hour12: true,
         timeStyle: "short",
@@ -39,7 +39,25 @@ export default function Home() {
 
             <div className="w-full h-auto flex flex-col items-center justify-start p-4">
 
-                {authStatus ? <p className="label text-xl">Hello,  <span className="text-xl px-1">{userData.name} 👋</span> </p> : <p className="w-full h-screen flex items-center justify-center"> Please Login </p>}
+                {authStatus ? <p className="label text-xl">Hello,  <span className="text-xl px-1">{userData.name} 👋</span> </p> : <p className="w-full h-screen flex flex-col items-center justify-start"> <TypeAnimation
+                    sequence={[
+                        'buggyWeather',
+                        1000,
+                        'Personalized weather app', //  Continuing previous Text
+                        1000,
+                        'Made on React',
+                        1000,
+                        'Made by Aditya (Astrobot)',
+                        1000,
+                    ]}
+                    style={{ fontSize: '4em' ,fontWeight:'semibold',marginTop:'40px',textAlign:"center"}}
+                    speed={1}
+                    deletionSpeed={10}
+                    repeat={Infinity}
+                    cursor={true}
+                />
+                <p>Please Login to Experience</p>
+                </p>}
                 {authStatus && (
 
                     <div className="flex flex-col items-center justify-start gap-4 w-full">
@@ -47,19 +65,19 @@ export default function Home() {
 
                         {weather == null && (
                             <div role="alert" className="alert alert-info w-[90%]">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              className="h-6 w-6 shrink-0 stroke-current">
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                            </svg>
-                            <span>There is no default Homecity set now, Please try Searching and set default</span>
-                          </div>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    className="h-6 w-6 shrink-0 stroke-current">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
+                                <span>There is no default Homecity set now, Please try Searching and set default</span>
+                            </div>
                         )}
                         {weather !== null && (
                             <CardFrame title={` Current Weather Stats for ${place} as of ${new Date(weather.dt * 1000).toLocaleString("en-IN", options)} [ HOMECITY ]`} width="w-[95%]" col={true}>
